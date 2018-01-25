e40ab89192291d63247024b1bd07ef7db3502401
Siva <shekrunk@users.noreply.github.com>
2018-01-25 17:07:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const path = require("path");
+
+
+var p1 = "source";//fullpath
+var arr1 = [];
+
+var p2 = "dest";//fullpath
+var arr2 = [];
+
+listContents(p1, arr1, readDir);
+listContents(p2, arr2, readDir);
+
+
+setTimeout(function () {
+    process();
+}, 1000);
+
+function listContents(dir, arr, callback) {
+    callback(dir, arr);
+}
+
+function readDir(dir, arr) {
+    fs.readdir(dir, function (err, files) {
+        if (err) {
+            throw err;
+        }
+        files.forEach(function (file) {
+            arr.push(file);
+        });
+    });
+}
+
+function process() {
+    var arr = arr2.filter(function (val) {
+        return !arr1.includes(val);
+    });
+    
+    arr.forEach(function (v) { 
+        console.log();
+        console.log(p2+v);
+
+        var dest = fs.createReadStream((p1+v),{ flags: 'w',  encoding: "binary",});
+        var src = fs.createReadStream((p2+v),{ flags: 'r',  encoding: "binary",});
+
+        src.pipe(dest, {end: false});
+    });
+    
+}
+
