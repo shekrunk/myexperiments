60a09f694b555bc6de31bbdf4a57e140ea733d39
Siva <shekrunk@users.noreply.github.com>
2018-05-09 11:19:14 +0530

    notifications with HTML5

---
+++ webnotifications/scripts/notifications.js
@@ -0,0 +1,28 @@
+
+function notify(){
+    if(!Notification) {
+        alert('Desktop Notifications not available in your browser');
+        return;
+    }
+
+    if(Notification.permission !== 'granted') {
+        Notification.requestPermission();
+        console.log('Requested Grant Permission for notification');
+    } else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification('Title', {icon: 'images/tweet.jpg', body: "Hey There!!! Poke! Poke! Poke!"});
+
+    notification.onclick = function() {
+        notification.close();
+        window.open('https://uat3.citibank.pl/apps/auth/signin/');
+    }
+
+    notification.onerror = function(err) {
+        console.error(err);
+    }
+  }
+    
+    
+    
+
+}
